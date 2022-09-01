b5d9b6b0cad131641050994ff637480018fdd595
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2022-09-01 19:02:37 +0300

    feature(player): add drum set play logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    document.addEventListener("keypress", getKeyCode, false);
+    document.addEventListener("keypress", playAudio, false);
 
-    function getKeyCode(event) {
-      console.log(`Key pressed = ${event.code} Keycode = ${event.keyCode}`);
+    function playAudio(event) {
+      let buttonPressed =
+        document.querySelector(`div[data-code="${event.keyCode}"]`) ||
+        document.querySelector(`div[data-keycode="${event.keyCode}"]`);
+
+      let audio =
+        document.querySelector(`audio[data-code="${event.keyCode}"]`) ||
+        document.querySelector(`audio[data-keycode="${event.keyCode}"]`);
+
+      buttonPressed.classList.toggle("currently-playing");
+      audio.play();
+      audio.currentTime = 0;
     }
   },
   false
